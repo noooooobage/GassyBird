187b3df9afd0affed381f631a5639cd9c48cdde6
Aidan Connor <ajconnor@email.wm.edu>
2021-03-19 22:10:13 -0400

    added methods for loading texture and sprite resources

--- src/ResourceCache.cpp
+++ src/ResourceCache.cpp
@@ -11,8 +11,8 @@
 #include "Globals.hpp"
 #include "ResourceCache.hpp"
 #include "Resource.hpp"
-#include "Resources/SpriteResource.hpp"
 #include "Resources/TextureResource.hpp"
+#include "Resources/SpriteResource.hpp"
 
 ResourceCache::ResourceCache() :
     _initialized(false)
@@ -29,24 +29,52 @@ void ResourceCache::init() {
     _initialized = true;
 
     // load test texture
-    sf::Texture testTexture;
-    testTexture.loadFromFile("../data/test_texture.png");
-    _resources.insert({"TEST_TEXTURE", std::make_shared<TextureResource>(testTexture)});
+    loadTextureResource(
+        "TEST_TEXTURE",
+        "../data/test_texture.png"
+    );
 
     // load test bird sprite
-    sf::Sprite testBirdSprite;
-    testBirdSprite.setTexture(getResource<TextureResource>("TEST_TEXTURE")->texture);
-    std::vector<sf::IntRect> textureRects = {
-        {0,  0, 7, 7},
-        {7,  0, 7, 7},
-        {14, 0, 7, 7},
-        {21, 0, 7, 7},
-        {0,  7, 7, 7},
-        {7,  7, 7, 7},
-        {14, 7, 7, 7},
-        {21, 7, 7, 7}
-    };
-    testBirdSprite.setTextureRect(textureRects.at(0));
-    _resources.insert({"TEST_BIRD_SPRITE",
-            std::make_shared<SpriteResource>(testBirdSprite, textureRects)});
+    loadSpriteResource(
+        "TEST_BIRD_SPRITE",
+        *getResource<TextureResource>("TEST_TEXTURE"),
+        {
+            {0,  0, 7, 7},
+            {7,  0, 7, 7},
+            {14, 0, 7, 7},
+            {21, 0, 7, 7},
+            {0,  7, 7, 7},
+            {7,  7, 7, 7},
+            {14, 7, 7, 7},
+            {21, 7, 7, 7}
+        }
+    );
+}
+
+void ResourceCache::loadTextureResource(const std::string& id, const std::string& filename) {
+
+    // load the texture from the file
+    sf::Texture texture;
+    texture.loadFromFile(filename);
+
+    // make sure a resource with the id does not already exist, then make the resource
+    assert(_resources.find(id) == _resources.end());
+    _resources[id] = std::make_shared<TextureResource>(texture);
+}
+
+void ResourceCache::loadSpriteResource(const std::string& id,
+        const TextureResource& textureResource, const std::vector<sf::IntRect>& textureRects) {
+
+    // create underlying sprite
+    sf::Sprite sprite;
+    sprite.setTexture(textureResource.texture);
+
+    // Make sure that there is at least one rectangle in textureRects, and set the sprite's
+    // texture rectangle to the first one.
+    assert(textureRects.size() > 0);
+    sprite.setTextureRect(textureRects.at(0));
+
+    // make sure a resource with the id does not already exist, then make the resource
+    assert(_resources.find(id) == _resources.end());
+    _resources[id] = std::make_shared<SpriteResource>(sprite, textureRects);
 }
