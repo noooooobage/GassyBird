46fed6f5b4f4e56c64936530383b75dda3c8efd4
Greg Quigg <grquigg@email.wm.edu>
2021-03-25 19:29:14 -0400

    Add FontResources and ArcadeClassic font

--- src/set_resource_types.cpp
+++ src/set_resource_types.cpp
@@ -5,6 +5,8 @@
 #include "Resource.hpp"
 #include "Resources/SpriteResource.hpp"
 #include "Resources/TextureResource.hpp"
+#include "Resources/FontResource.hpp"
 
 const ResourceType TextureResource::TYPE = (ResourceType)&TextureResource::TYPE;
 const ResourceType SpriteResource::TYPE = (ResourceType)&SpriteResource::TYPE;
+const ResourceType FontResource::TYPE = (ResourceType)&FontResource::TYPE;
